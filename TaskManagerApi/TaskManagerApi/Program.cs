a6ce7c9773c574c5802932fa9e803b27ef1c7e0e
Clenildon Ferreira <clenildonferreira34@gmail.com>
2025-04-01 11:51:16 -0300

    feat: cria modelo TaskItem e configura DbContext com MySQL

--- TaskManagerApi/TaskManagerApi/Program.cs
+++ TaskManagerApi/TaskManagerApi/Program.cs
@@ -1,23 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 41)))); 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
