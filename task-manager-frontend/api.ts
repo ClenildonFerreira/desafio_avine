fdaaf80d918dd53dead458aa77fc6b27283d8229
Clenildon Ferreira <clenildonferreira34@gmail.com>
2025-04-02 13:32:46 -0300

    formulario criado

--- task-manager-frontend/api.ts
+++ task-manager-frontend/api.ts
@@ -1,5 +1,5 @@
 import { ITask } from "@/types/task";
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 const baseUrl = "http://localhost:14276/api"; 
 
@@ -8,8 +8,31 @@ export const getAllTodos = async (): Promise<ITask[]> => {
     const res = await axios.get(`${baseUrl}/Task`);
     return res.data;
   } catch (error) {
-    console.error('Erro ao buscar tarefas', error);
+    const err = error as AxiosError;
+    console.error('Erro ao buscar a tarefas:', err.message);
     return [];
   }
+};
+
+export const getTodoById = async (id: number): Promise<ITask | null> => {
+  try {
+    const response = await axios.get<ITask>(`${baseUrl}/Task/${id}`);
+    return response.data;
+  } catch (error) {
+    const err = error as AxiosError;
+    console.error('Erro ao buscar a tarefa:', err.message);
+    return null;
+  }
+};
+
+export const addTodo = async (task: ITask): Promise<ITask | null> => {
+    try {
+      const res = await axios.post<ITask>(`${baseUrl}/Task`, task);
+      return res.data;
+    } catch (error) {
+      const err = error as AxiosError;
+      console.error("Erro ao criar a tarefa:", err.message);
+      return null;
+    }
 };
   
--- task-manager-frontend/app/components/AddTask.tsx
+++ task-manager-frontend/app/components/AddTask.tsx
@@ -1,13 +1,87 @@
+"use client";
+
 import { AiOutlinePlus } from "react-icons/ai";
+import Modal from "./Modal";
+import { FormEventHandler, useState } from "react";
+import { addTodo } from "@/api";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 
 const AddTask = () => {
+  const router = useRouter();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [dueDate, setDueDate] = useState<string>("");
+  const [completed, setCompleted] = useState<boolean>(false);
+
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    await addTodo({
+      id: uuidv4(),
+      title,
+      description,
+      dueDate,
+      completed,
+    });
+
+    setTitle("");
+    setDescription("");
+    setDueDate("");
+    setCompleted(false);
+    setModalOpen(false);
+
+    router.refresh();
+  };
+
   return (
     <div>
-        <button
-        className='btn btn-primary w-full'
-        >Add nova tarefa 
-        <AiOutlinePlus className='ml-2' size={18} />
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-primary w-full"
+      >
+        Adicionar nova tarefa <AiOutlinePlus className="ml-2" size={18} />
       </button>
+
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form onSubmit={handleSubmitNewTodo}>
+          <h3 className="font-bold text-lg">Adicionar nova tarefa</h3>
+          <div className="modal-action flex flex-col gap-4">
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              placeholder="Título da tarefa"
+              className="input input-bordered w-full"
+            />
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Descrição da tarefa"
+              className="input input-bordered w-full my-2"
+            />
+            <input
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
+              type="date"
+              className="input input-bordered w-full my-2"
+            />
+            <label>
+              Concluída:
+              <input
+                type="checkbox"
+                checked={completed}
+                onChange={() => setCompleted(!completed)}
+                className="checkbox ml-2"
+              />
+            </label>
+            <button type="submit" className="btn btn-secondary mt-4">
+              Enviar
+            </button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };
